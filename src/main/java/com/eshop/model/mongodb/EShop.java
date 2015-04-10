61d0587f284f2f63721eb010ec6bfab8b0640e20
khiker <khiker@khiker-PC>
2015-04-10 15:53:07 +0800

    经纬度集合，可方便位置计算及查找

--- src/main/java/com/eshop/model/mongodb/EShop.java
+++ src/main/java/com/eshop/model/mongodb/EShop.java
@@ -12,11 +12,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.eshop.frameworks.core.entity.MongoEntity;
 
-
-@Document(collection="e_shop")
+@Document(collection = "e_shop")
 @Data
 @EqualsAndHashCode(callSuper = false)
-public class EShop extends MongoEntity{
+public class EShop extends MongoEntity {
 
 	/**
 	 * 
@@ -25,44 +24,43 @@ public class EShop extends MongoEntity{
 
 	@Id
 	private String userId;
-	
-	@Indexed(unique=false,name="shop_address",direction=IndexDirection.ASCENDING)
+
+	@Indexed(unique = false, name = "shop_address", direction = IndexDirection.ASCENDING)
 	private String shopAddress;
-	
-	@Indexed(unique=false,name="shop_name",direction=IndexDirection.ASCENDING)
+
+	@Indexed(unique = false, name = "shop_name", direction = IndexDirection.ASCENDING)
 	private String shopName;
-	
+
 	private Integer isActivted;
-	
-	@Indexed(unique=false,name="shop_photo",direction=IndexDirection.ASCENDING)
+
+	@Indexed(unique = false, name = "shop_photo", direction = IndexDirection.ASCENDING)
 	private String shopPhoto;
-	
-	@Indexed(unique=false,name="city_code",direction=IndexDirection.ASCENDING)
+
+	@Indexed(unique = false, name = "city_code", direction = IndexDirection.ASCENDING)
 	private String cityCode;
-	
+
 	private Date regTime;
-	
-	@Indexed(unique=false,name="is_online",direction=IndexDirection.ASCENDING)
+
+	@Indexed(unique = false, name = "is_online", direction = IndexDirection.ASCENDING)
 	private Integer isOnline;
-	
+
 	private Integer verified;
-	
-	@Indexed(unique=false,name="devliver_scope",direction=IndexDirection.ASCENDING)
+
+	@Indexed(unique = false, name = "devliver_scope", direction = IndexDirection.ASCENDING)
 	private Double devliverScope;
-	
-	//TODO 已经删除本字段，考虑到同步代码时需要同步数据库，你们那边会报错，所以本字段暂时未删除
-	@Indexed(unique=false,name="shopPoint",direction=IndexDirection.ASCENDING)
+
+	// TODO 已经删除本字段，考虑到同步代码时需要同步数据库，你们那边会报错，所以本字段暂时未删除
+	@Indexed(unique = false, name = "shopPoint", direction = IndexDirection.ASCENDING)
 	private String shopPoint;
-	
-	@Indexed(unique=false,name="shop_lng",direction=IndexDirection.ASCENDING)
-	private Double lng;//经度
-	
-	@Indexed(unique=false,name="shop_lat",direction=IndexDirection.ASCENDING)
-	private Double lat;//纬度
-	
-	
+	// 可用此bean代替下面经纬度，加快搜索
+	// @GeoSpatialIndexed//声明该字段为地理信息的索引
+	// private LatLonPoint latlon;
+	@Indexed(unique = false, name = "shop_lng", direction = IndexDirection.ASCENDING)
+	private Double lng;// 经度
+
+	@Indexed(unique = false, name = "shop_lat", direction = IndexDirection.ASCENDING)
+	private Double lat;// 纬度
+
 	private String remark;
 
-	
-	
 }
--- src/main/java/com/eshop/model/mongodb/EUserAddress.java
+++ src/main/java/com/eshop/model/mongodb/EUserAddress.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexed;
 import org.springframework.data.mongodb.core.index.IndexDirection;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -34,8 +35,9 @@ public class EUserAddress extends MongoEntity{
 	
 	@Indexed(unique=false,name="address_mobile",direction=IndexDirection.ASCENDING)
 	private String mobile;
-	
-
+	//可用此bean代替下面经纬度，加快搜索
+	//@GeoSpatialIndexed//声明该字段为地理信息的索引
+	//private LatLonPoint latlon;
 	@Indexed(unique=false,name="address_lng",direction=IndexDirection.ASCENDING)
 	private Double lng;//经度
 	
